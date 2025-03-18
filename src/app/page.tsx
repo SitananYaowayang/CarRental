08e5e9a1318946e48af8bd401ca74b0b856ac8ff
SitananYaowayang <6733270421@student.chula.ac.th>
2025-03-18 15:19:16 +0700

    W08_Hooks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,13 @@ import styles from "./page.module.css";
 import Banner from "@/components/Banner";
 import Productcard from "@/components/ProductCard";
 import CarPanel from "@/components/CarPanel";
+import { TravelCard } from "@/components/TravelCard";
 
 export default function Home() {
   return (
     <main >
       <Banner/>
-      
+      <TravelCard/>
     </main>
   );
 }
---
+++ src/components/VlogPlayer.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { useRef, useState } from "react"
+import { useEffect } from "react"
+import { useWindowListener } from "@/hooks/useWindowListener"
+export function VlogPlayer(
+    {vdoSrc,isPlaying}:{vdoSrc:string,isPlaying:boolean}
+){
+    const vdoRef = useRef<HTMLVideoElement>(null)
+    useEffect(()=>{
+        // alert('width is ' + vdoRef.current?.videoWidth)
+        if(isPlaying){
+            // alert('Play')
+            vdoRef.current?.play()
+        }
+        else{
+            // alert('Pause')
+            vdoRef.current?.pause()
+        }
+    },[isPlaying])
+    
+   useWindowListener("resize",(e)=>{alert('Window width is'+ (e.target as Window).innerWidth)})
+    
+    return(
+        <video className="w-[40%]" src={vdoSrc} ref={vdoRef} 
+        muted loop controls/>
+    )
+}
