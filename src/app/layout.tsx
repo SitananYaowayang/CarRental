ffd5f34a3bc55112d17e68bbd824c6af1006fe40
SitananYaowayang <6733270421@student.chula.ac.th>
2025-03-22 11:13:05 +0700

    W10_debug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,9 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
 
   const nextAuthSession = await getServerSession(authOptions)  
 
